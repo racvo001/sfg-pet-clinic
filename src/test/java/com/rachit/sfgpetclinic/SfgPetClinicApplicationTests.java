53f085b7952692538b85149a57ed49a7b155ba2b
rachit.gupta <rachit.gupta@asurion.com>
2019-11-08 10:43:55 -0800

    Initial Start Of Project

---
+++ src/test/java/com/rachit/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachit.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
